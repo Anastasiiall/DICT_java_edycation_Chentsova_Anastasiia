fcef2993dc2722b6a4169900eb09b8d895c8ba96
Anastasiia11 <a.p.chentsova@student.khai.edu>
2022-10-05 09:20:16 +0300

    Resolved conflict #3

--- src/Test.java
+++ src/Test.java
@@ -5,6 +5,10 @@ public class Test {
 =======
         System.out.println("education");
         //new string
+<<<<<<< HEAD
 >>>>>>> 62eec28 (Test.java added to feature_branch with change on string #2)
+=======
+        System.out.println("DICT");
+>>>>>>> 6de9f8f (Test.java added to feature_branch with change on string #3)
     }
 }
