8523f3ccd1f5217c053a51789123ab31404c4c1f
Anastasiia11 <a.p.chentsova@student.khai.edu>
2022-10-20 10:09:49 +0300

    "Resolved conflict #4"

--- src/Test.java
+++ src/Test.java
@@ -1,14 +1,7 @@
 public class Test {
     public static void main(String[] args){
-<<<<<<< HEAD
         System.out.println("Java education");
-=======
-        System.out.println("education");
         //new string
-<<<<<<< HEAD
->>>>>>> 62eec28 (Test.java added to feature_branch with change on string #2)
-=======
         System.out.println("DICT");
->>>>>>> 6de9f8f (Test.java added to feature_branch with change on string #3)
     }
 }
