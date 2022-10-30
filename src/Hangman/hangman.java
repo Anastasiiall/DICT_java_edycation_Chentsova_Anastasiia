dab19acdbf11923ea0e400e921b2880b6008bd87
Anastasiia11 <a.p.chentsova@student.khai.edu>
2022-10-30 18:04:24 +0200

    4-th stage

--- src/Hangman/hangman.java
+++ src/Hangman/hangman.java
@@ -17,11 +17,25 @@ public class hangman {
 
         Random randomizer = new Random();
         String randomListElement = list.get(randomizer.nextInt(list.size()));
-        
+        int randomWordLength = randomListElement.length();
 
+        String let1 = String.valueOf(randomListElement.charAt(0));
+        String let2 = String.valueOf(randomListElement.charAt(1));
+
+        int a = 0;
+        int b = randomWordLength - 2;
+        String c = "-";
         Scanner word = new Scanner(System.in);
         String YourWord;
-        System.out.println("Guess the word:");
+        System.out.print("Guess the word ");
+        System.out.print(let1);
+        System.out.print(let2);
+        while (a < b){
+            a++;
+            System.out.print(c);
+        }
+        System.out.println(": ");
+
         YourWord = word.nextLine();
         if (Objects.equals(YourWord, randomListElement)){
             System.out.println("You survived!");
